419e9fc4e947cb24fe7e1cb4b420562368467212
demokal <carmonaagustin@gmail.com>
2026-09-16 22:54:46 -0300

    primer entrega

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let Apellido = prompt("Ingresa tu apellido:");
+console.log(Apellido);
+
+let nombre = prompt("Ingresa tu nombre:");
+console.log("Hola " + nombre + "!");
+
+const veinte = 2026;
+
+let año = Number(prompt("Ingresa tu año de nacimiento:"));
+console.log(veinte - año);
