3627a12caada6476da2a0d299be9914544664b4e
demokal <carmonaagustin@gmail.com>
2026-09-24 22:18:48 -0300

    se creo un loop utilizando condicionales al main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-let Apellido = prompt("Ingresa tu apellido:");
-console.log(Apellido);
+// let Apellido = prompt("Ingresa tu apellido:");
+// console.log(Apellido);
 
-let nombre = prompt("Ingresa tu nombre:");
-console.log("Hola " + nombre + "!");
+// let nombre = prompt("Ingresa tu nombre:");
+// console.log("Hola " + nombre + "!");
 
 const veinte = 2026;
 
 let año = Number(prompt("Ingresa tu año de nacimiento:"));
 console.log(veinte - año);
+
+let email = prompt("Ingresa tu Email:");
+let contraseña = prompt("Ingresa tu contraseña:");
+alert("Email:Dilan@gmail.com\nContraseña:1234");
+
+let intentos = 1;
+while (intentos < 3) {
+    email = prompt("Ingresa tu Email:");
+    contraseña = prompt("Ingresa tu contraseña:");
+    intentos++;
+}
+
+if (email === "Dilan@gmail.com" && contraseña === "1234") {
+    console.log("¡Bienvenido, " + email + "!");
+} else{
+    console.log("Email o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+}
